ff267a128804a3269761a8f8f769a3c723c0a1da
yassine-myterial <121604599+yassine-myterial@users.noreply.github.com>
2022-12-30 23:29:44 +0100

    possibility to scale multiple services at once

--- app.js
+++ app.js
@@ -9,56 +9,54 @@ const app = express();
 const {execSync} = require('child_process');
 
 let {KEY_FILE_PATH} = process.env;
-let {PROJECT_ID} = process.env;
 
 // This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
 // [END run_pubsub_server_setup]
 // [END cloudrun_pubsub_server_setup]
 
-// [START cloudrun_pubsub_handler]
-// [START run_pubsub_handler]
 try{
   app.post('/', (req, res) => {
     if (!req.body) {
-      const msg = 'no Pub/Sub message received';
+      const msg = 'no config received';
       console.error(`error: ${msg}`);
       res.status(400).send(`Bad Request: ${msg}`);
       return;
     }
 
-    if (!req.body.message) {
-      const msg = 'invalid Pub/Sub message format';
+    if (!Array.isArray(req.body) {
+      const msg = 'invalid config format';
       console.error(`error: ${msg}`);
       res.status(400).send(`Bad Request: ${msg}`);
       return;
     }
 
-    const message = req.body.message;
-    const cpuThrotting= message.cpuThrotting? '--cpu-throttling': '--no-cpu-throttling';
-    console.log(message);
+    const configsArray = req.body;
+    
+    for(const config of configsArray){
+      const cpuThrotting= config.cpuThrotting? '--cpu-throttling': '--no-cpu-throttling';
+      let gcloudCmd =
+      `gcloud auth activate-service-account myterial-pipeline-user@myterial-dev.iam.gserviceaccount.com ` +
+      `--key-file="${KEY_FILE_PATH}" `;
 
-    let gcloudCmd =
-        `gcloud auth activate-service-account myterial-pipeline-user@myterial-dev.iam.gserviceaccount.com ` +
-        `--key-file="${KEY_FILE_PATH}" `;
-
-    console.log('Starting scaling');
-    execSync(gcloudCmd).toString(); 
-    gcloudCmd =
-        `gcloud run services update ${message.service} ` +
-        `--concurrency=${message.scaling.concurrency} ` +
-        `--min-instances=${message.scaling.from} ` +
-        `--max-instances=${message.scaling.to} ` +
-        `${cpuThrotting} ` +
-        `--region=europe-west3  ` +
-        `--project "${message.projectId}" `;
-    execSync(gcloudCmd).toString(); 
-    console.log('Scaling completed.');
-    res.status(204).send();
+      console.log('Starting scaling');
+      execSync(gcloudCmd).toString(); 
+      gcloudCmd =
+          `gcloud run services update ${config.service} ` +
+          `--concurrency=${config.scaling.concurrency} ` +
+          `--min-instances=${config.scaling.from} ` +
+          `--max-instances=${config.scaling.to} ` +
+          `${cpuThrotting} ` +
+          `--region=europe-west3  ` +
+          `--project "${config.projectId}" `;
+      execSync(gcloudCmd).toString(); 
+      console.log('Scaling completed.');
+      
+    }
+    res.status(204).send();    
   });
-// [END run_pubsub_handler]
-// [END cloudrun_pubsub_handler]
 }catch(error){
+  res.status(500).send();
   console.log(error);
 }
 module.exports = app;
