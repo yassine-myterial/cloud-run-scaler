39c83df54fb014cb2b35cc77844e99f3737da797
yassine-myterial <121604599+yassine-myterial@users.noreply.github.com>
2022-12-30 12:01:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 // [START run_pubsub_server_setup]
 const express = require('express');
 const app = express();
+const {execSync} = require('child_process');
 
 // This middleware is available in Express v4.16.0 onwards
 app.use(express.json());
@@ -21,18 +22,27 @@ app.post('/', (req, res) => {
     res.status(400).send(`Bad Request: ${msg}`);
     return;
   }
+  
   if (!req.body.message) {
     const msg = 'invalid Pub/Sub message format';
     console.error(`error: ${msg}`);
     res.status(400).send(`Bad Request: ${msg}`);
     return;
   }
-
+  
   const pubSubMessage = req.body.message;
   const name = pubSubMessage.data
     ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
     : 'World';
-
+  /*
+  let gcloudCmd =
+      `gcloud auth activate-service-account myterial-pipeline-user@myterial-dev.iam.gserviceaccount.com ` +
+      `--key-file="${KEY_FILE_PATH}" `;
+      */
+  let gcloudCmd ="gcloud version";
+  console.log('Starting scaling');
+  execSync(gcloudCmd, {timeout: 10 * 1000}); // timeout at 4 mins
+  console.log('Scaling completed.');
   console.log(`Hello ${name}!`);
   res.status(204).send();
 });
